3091327dee07e5d4923c7d148cec1cca4a11565d
dotcs <git@dotcs.me>
2020-11-16 21:19:18 +0100

    Use server-side code for code highlighting in posts

--- components/Markdown/index.tsx
+++ components/Markdown/index.tsx
@@ -1,13 +1,28 @@
 import { FC } from 'react';
 import MarkdownIt from 'markdown-it';
 import markdownItTocAndAnchor from "markdown-it-toc-and-anchor"
+import hljs from 'highlight.js';
 
 export interface MarkdownProps {
     text: string;
     className?: string;
 }
 
-const md = new MarkdownIt().use(markdownItTocAndAnchor, {
+const md = new MarkdownIt({
+    html: true,
+    langPrefix: 'language-',
+    highlight: function (str: string, lang: string) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+               hljs.highlight(lang, str, true).value +
+               '</code></pre>';
+      } catch (__) {}
+    }
+ 
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  }
+}).use(markdownItTocAndAnchor, {
     anchorLinkSymbol: '#',
     anchorLinkSymbolClassName: 'hidden sm:inline-block x-headline-anchor',
 });
