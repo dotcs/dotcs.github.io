f772858a0ecebf8e9c3302c7ea675cdf11f7cf33
dotcs <git@dotcs.me>
2020-11-16 20:53:13 +0100

    Show headline anchors

--- components/Markdown/index.tsx
+++ components/Markdown/index.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react';
 import MarkdownIt from 'markdown-it';
+import markdownItTocAndAnchor from "markdown-it-toc-and-anchor"
 
 export interface MarkdownProps {
     text: string;
     className?: string;
 }
 
-const md = new MarkdownIt();
+const md = new MarkdownIt().use(markdownItTocAndAnchor, {
+    anchorLinkSymbol: '#',
+    anchorLinkSymbolClassName: 'hidden sm:inline-block x-headline-anchor',
+});
 
 const Markdown: FC<MarkdownProps> = (props) => {
     const html = md.render(props.text);
