85f4b0e33eea9b0843c128253a3d40862039f864
dotcs <git@dotcs.me>
2020-11-17 10:04:52 +0100

    Purge unused css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-      plugins: [require('tailwindcss'), require('postcss-preset-env')],
+    plugins: {
+        'tailwindcss': {},
+        'postcss-preset-env': {},
+        '@fullhuman/postcss-purgecss': {
+            content: [
+                './components/*.{ts,tsx}',
+                './components/**/*.{ts,tsx}',
+                './pages/*.{ts,tsx}',
+                './pages/**/*.{ts,tsx}',
+            ],
+            safelist: [
+                // Custom component definitions
+                /^x-/,
+
+                // Plugins
+                /^hljs-?/, // highlight.js css rules
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        }
+    }
 }
