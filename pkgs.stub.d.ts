b67e7915cff2833e335037bbf459f99f6bb953ed
dotcs <git@dotcs.me>
2020-11-21 21:19:32 +0100

    Add docstring

--- pkgs.stub.d.ts
+++ pkgs.stub.d.ts
@@ -1,2 +1,7 @@
+/**
+ * This file should only list packages and type definitions for packages that
+ * don't provide their own typings and which don't have any types available via
+ * the @types-repository.
+ */
 declare module 'markdown-it-toc-and-anchor';
 declare module 'markdown-it-footnote';
